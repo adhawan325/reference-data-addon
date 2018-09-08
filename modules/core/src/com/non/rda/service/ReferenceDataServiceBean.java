52d267d61be89a41894ee5e76448922285b47d9b
Ankur Dhawan <adhawan@SHIPLETS.AWSDIRECTORY.COM>
2018-09-08 07:20:55 -0700

    cleanup

--- modules/core/src/com/non/rda/service/ReferenceDataServiceBean.java
+++ modules/core/src/com/non/rda/service/ReferenceDataServiceBean.java
@@ -1,9 +1,8 @@
 package com.non.rda.service;
 
-import com.haulmont.cuba.core.EntityManager;
 import com.haulmont.cuba.core.Persistence;
-import com.haulmont.cuba.core.Query;
-import com.haulmont.cuba.core.Transaction;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
 import com.non.rda.entity.ReferenceData;
 import org.springframework.stereotype.Service;
 
@@ -14,40 +13,27 @@ import java.util.List;
 public class ReferenceDataServiceBean implements ReferenceDataService {
     @Inject
     private Persistence persistence;
+    @Inject
+    private DataManager dataManager;
 
     @Override
     public ReferenceData getReferenceDataByCode(String code) {
-        Transaction tx = persistence.createTransaction();
-        EntityManager entityManager = persistence.getEntityManager();
-        Query query = entityManager
-                .createQuery("select e from nonrda$ReferenceData e where e.referenceCode = :code order by e.sortOrder");
-        query.setParameter("code", code);
-        ReferenceData referenceData = (ReferenceData) query.getFirstResult();
-        tx.end();
-        return referenceData;
+        LoadContext<ReferenceData> loadContext = LoadContext.create(ReferenceData.class);
+        loadContext.setQuery(new LoadContext.Query("select e from nonrda$ReferenceData e where e.referenceCode = :code order by e.sortOrder").setParameter("code", code).setCacheable(true));
+        return dataManager.load(loadContext);
     }
 
     @Override
-    public List<ReferenceData> getReferenceDataListByCategory(String category)
-    {
-        Transaction tx = persistence.createTransaction();
-        EntityManager entityManager = persistence.getEntityManager();
-        Query query = entityManager.createQuery("select e from nonrda$ReferenceData e where e.referenceCategory = :category order by e.sortOrder");
-        query.setParameter("category", category);
-        List<ReferenceData> referenceValues = query.getResultList();
-        return referenceValues;
+    public List<ReferenceData> getReferenceDataListByCategory(String category) {
+        LoadContext<ReferenceData> loadContext = LoadContext.create(ReferenceData.class);
+        loadContext.setQuery(new LoadContext.Query("select e from nonrda$ReferenceData e where e.referenceCategory = :category order by e.sortOrder").setParameter("category", category).setCacheable(true));
+        return dataManager.loadList(loadContext);
     }
 
     @Override
-    public List<ReferenceData> getReferenceDataListByParent(ReferenceData parent)
-    {
-        Transaction tx = persistence.createTransaction();
-        EntityManager entityManager = persistence.getEntityManager();
-        Query query = entityManager.createQuery("select e from nonrda$ReferenceData e where e.parent.id = :parentId order by e.sortOrder");
-        query.setParameter("parentId", parent.getId());
-        List<ReferenceData> referenceValues = query.getResultList();
-        return referenceValues;
-
+    public List<ReferenceData> getReferenceDataListByParent(ReferenceData parent) {
+        LoadContext<ReferenceData> loadContext = LoadContext.create(ReferenceData.class);
+        loadContext.setQuery(new LoadContext.Query("select e from nonrda$ReferenceData e where e.parent.id = :parentId order by e.sortOrder").setParameter("parentId", parent.getId()).setCacheable(true));
+        return dataManager.loadList(loadContext);
     }
-
 }
